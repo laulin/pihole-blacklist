903bbecd9b7770779c295c506fd2b118070a86e4
laulin <laulin@nomail.com>
2020-08-25 18:18:22 +0200

    add update script.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+# get
+python3 get_blacklists.py
+python3 get_top_1000000.py
+
+# commit
+git add blacklist.txt
+git commit -m "automatic blacklist update"
+
+git add top_1000000.txt
+git commit -m "automatic top 1M update"
+
+# push
+git push
